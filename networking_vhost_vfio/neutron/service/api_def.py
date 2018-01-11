ffe3911ea2a00392ab5ad027462f4bd2e5b77e70
David Shaughnessy <david.shaughnessy@intel.com>
2018-01-11 10:22:29 +0000

    Create stub for neutron extension
    Create a basic Neutron extension
    - introduces an API definition file for the service plugin.
    - a base neutron service plugin which contains necessary metadata to
      extend neutron's API.
    - a hollow l2 extension which will be extended with functionality
      in a future patch.
    
    Change-Id: I16b49e92bf69929bd08f4b33cf5aa0c45fdd249c

---
+++ networking_vhost_vfio/neutron/service/api_def.py
@@ -0,0 +1,26 @@
+# 2017 - 2018 Intel Corporation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+ALIAS = 'vfio'
+IS_SHIM_EXTENSION = False
+IS_STANDARD_ATTR_EXTENSION = False
+NAME = 'Networking Vhost Vfio'
+API_PREFIX = '/' + ALIAS
+DESCRIPTION = 'The Networking Vhost Vfio extension.'
+UPDATED_TIMESTAMP = '2018-01-09T10:00:00-00:00'
+RESOURCE_ATTRIBUTE_MAP = {}
+ACTION_MAP = {}
+REQUIRED_EXTENSIONS = []
+OPTIONAL_EXTENSIONS = []
+ACTION_STATUS = {}
---
+++ networking_vhost_vfio/neutron/service/plugin_base.py
@@ -0,0 +1,49 @@
+# 2017 - 2018 Intel Corporation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import abc
+import six
+
+from networking_vhost_vfio.neutron.service import api_def
+
+from neutron_lib.api import extensions as api_extensions
+from neutron_lib.services import base as service_base
+
+
+class VhostVfio(api_extensions.APIExtensionDescriptor):
+
+    api_definition = api_def
+
+    @classmethod
+    def get_plugin_interface(cls):
+        return VhostVfioServicePluginBase
+
+    def get_extended_resources(self, version):
+        if version == "2.0":
+            return dict(list(api_def.RESOURCE_ATTRIBUTE_MAP.items()))
+        else:
+            return {}
+
+
+@six.add_metaclass(abc.ABCMeta)
+class VhostVfioServicePluginBase(service_base.ServicePluginBase):
+
+    prefix_path = api_def.API_PREFIX
+
+    def get_plugin_description(self):
+        return "Plugin to update the Placement API with vfio states"
+
+    @classmethod
+    def get_plugin_type(cls):
+        return "VFIO"
