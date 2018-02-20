9e05e2d201312d1803ae57c7a16e41aac666bac7
helena mcgough <helena.mcgough@intel.com>
2018-02-20 09:04:26 +0000

    Updated devstack plugin(Work-in-progress).
     - Configures and installs OVS, QEMU and additional drivers
     - Add sample local.conf file
    
    Change-Id: I8e4acaeedb2224708d3f9ff12579014aedcd01ea

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -15,12 +15,19 @@ ERROR_ON_CLONE=False
             case $2 in
                 "pre-install")
                     # cloning source code
-                        echo_summary "Cloning of src files for vhost-vfio not required"
+                    echo_summary "Cloning of src files for vhost-vfio not required"
                 ;;
                 "install")
                     # Perform installation of vhost-vfio
                     sudo -EH yum install -y crudini || sudo -EH apt-get install -y crudini
                     sudo pip install -e "${NETWORKING_VHOST_VFIO_DIR}"
+
+                    if [ $VHOST_VFIO_INSTALL == 'True' ]; then
+                        echo_summary "Configuring, installing and starting OVS"
+                        install_networking_vhost_vfio
+                    else
+                        echo_summary "networking_vhost_vfio will not be installed"
+                    fi
                 ;;
                 "post-config")
                     vhost_vfio_config_update
