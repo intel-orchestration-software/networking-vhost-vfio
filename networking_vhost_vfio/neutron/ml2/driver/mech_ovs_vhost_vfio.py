06cda9e08757a45721de2deeab408452af72878c
Helena McGough <helena.mcgough@intel.com>
2018-03-27 09:39:50 +0100

    Updated Devstack plugin.
     - Updated branch information throughout the plugin.
     - Included a script to prevent rebinding of VFs to virtio_pci driver.
     - Updated allocatation of VFs service.
    
    Change-Id: Ib338546b850981271db96eb9ee38fd3ab6df02a1

--- networking_vhost_vfio/neutron/ml2/driver/mech_ovs_vhost_vfio.py
+++ networking_vhost_vfio/neutron/ml2/driver/mech_ovs_vhost_vfio.py
@@ -12,64 +12,18 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-
-from neutron_lib.api.definitions import portbindings
-
-from neutron.agent import securitygroups_rpc
-from neutron.plugins.ml2.drivers.openvswitch.agent.common \
-    import constants as a_const
 from neutron.plugins.ml2.drivers.openvswitch.mech_driver \
     import mech_openvswitch as ml2d
 
-AGENT_TYPE = "ovs-vhost-vfio"
+AGENT_TYPE = "virtio-direct"
 
 
-class OVSvHostVFIOMechanismDriver(ml2d.OpenvswitchMechanismDriver):
+class OVSVirtioDirectMechanismDriver(ml2d.OpenvswitchMechanismDriver):
 
     def __init__(self):
-        super(OVSvHostVFIOMechanismDriver, self).__init__()
-        sg_enabled = securitygroups_rpc.is_firewall_enabled()
-        hybrid_plug_required = False
+        super(OVSVirtioDirectMechanismDriver, self).__init__()
         self.agent_type = AGENT_TYPE
-        self.vif_details = {
-            portbindings.CAP_PORT_FILTER: sg_enabled,
-            portbindings.OVS_HYBRID_PLUG: hybrid_plug_required}
 
-    def get_vif_type(self, context, agent, segment):
-        a_config = agent['configurations']
-        datapath = a_config.get(
-            'datapath_type', a_const.OVS_DATAPATH_SYSTEM)
-        return (portbindings.VIF_TYPE_VHOST_VFIO if
-                datapath == a_const.OVS_DATAPATH_SYSTEM
-                else portbindings.VIF_TYPE_OVS)
-
-    def _pre_get_vif_details(self, agent, context):
-        a_config = agent['configurations']
-        vif_type = self.get_vif_type(context, agent, segment=None)
-        if vif_type != portbindings.VIF_TYPE_VHOST_VFIO:
-            details = dict(self.vif_details)
-            hybrid = portbindings.OVS_HYBRID_PLUG
-            if hybrid in a_config:
-                # we only override the vif_details for hybrid plugging set
-                # in the constructor if the agent specifically requests it
-                details[hybrid] = a_config[hybrid]
-        else:
-            mdev_path = self.mdev_path(agent, context.current['id'])
-            profile = context.current.get(portbindings.PROFILE)
-            type = profile.get(portbindings.VHOST_VFIO_MDEV_TYPE)
-            parent = profile.get(portbindings.VHOST_VFIO_PARENT_DEVICE)
-            details = {portbindings.CAP_PORT_FILTER: False,
-                       portbindings.OVS_HYBRID_PLUG: False,
-                       portbindings.VHOST_VFIO_MDEV_PATH: mdev_path,
-                       portbindings.VHOST_VFIO_PARENT_DEVICE: parent,
-                       portbindings.VHOST_VFIO_MDEV_TYPE: type}
-        details[portbindings.OVS_DATAPATH_TYPE] = a_config.get(
-            'datapath_type', a_const.OVS_DATAPATH_SYSTEM)
-        return details
-
-    @staticmethod
-    def mdev_path(agent, port_id):
-        """Return the mdev path for a given port"""
-        basedir = "/sys/bus/mdev/devices/"
-        return os.path.join(basedir, port_id)
+    def bind_port(self, context):
+        #NOTE this intentionally does not call it's immediate parent.
+        super(ml2d.OpenvswitchMechanismDriver, self).bind_port(context)
